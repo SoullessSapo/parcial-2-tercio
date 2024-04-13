c1e85a9b6639a5f6f231f2a84294a3734c82dc31
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2024-04-13 09:18:25 -0500

    Update GreetingController.java

--- src/main/java/co/edu/escuelaing/cvds/lab7/controller/GreetingController.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/controller/GreetingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value = "/greetings")
+@RequestMapping(value = "/greeting")
 public class GreetingController {
     private final ToDoService toDoService;
 
